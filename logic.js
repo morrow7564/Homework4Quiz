836b8a42bdeaae99c6000326ffed068afe3833d1
morrow7564 <morrow7564@gmail.com>
2020-09-24 09:36:49 -0400

    adding HW4

--- logic.js
+++ logic.js
@@ -1,36 +1,133 @@
-var currentQuestionIndex = 0
+// Var from HTML
+var startBtn = document.getElementById("startBtn")
+var questions = document.getElementById("questions")
+var boxQuestions = document.getElementById("boxQuestions")
+var timer = document.getElementById("timer")
+var timeSec = 30;
+var nameholder = document.getElementById("nameholder");
+var par = document.getElementById("par")
+var timeLeft = document.getElementById("timeLeft");
 
 
 
-function startQuiz() {
-    var startScreenEl = document.getElementById("start-screen")
+// questions for the quiz
+var questions = [
+    {
+        question: "Where do we put the JavaScript link in HTML?",
+        answer1: "Head",
+        answer2: "Body",
+        answer3: "Div",
+        answer4: "Header",
+        rightAns: "1"
+    }, {
+        question: "What is a JavaScript keyword that refers to the object?",
+        answer1: "That",
+        answer2: "Symbol",
+        answer3: "String",
+        answer4: "This",
+        rightAns: "4"
+    },
+    {
+        question: "How do you start a FOR loop?",
+        answer1: "for i = 1 to 3",
+        answer2: "for (i <= 3; i++)",
+        answer3: "for (i = 0; i <= 3; i++)",
+        answer4: "for (i = 0; i <= 3)",
+        rightAns: "3"
+    }, {
+        question: "How would you create a function in JavaScript?",
+        answer1: "function = myFunction()",
+        answer2: "function myFunction()",
+        answer3: "function:myFunction()",
+        answer4: "function myFunction{}",
+        rightAns: "2"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answer1: "=",
+        answer2: "+",
+        answer3: "*",
+        answer4: "%",
+        rightAns: "1"
+    },
+]
 
-    // hide start screen once start button is clicked
+// adding questions to a function
+function quest() {
+    var ans = questions[showing];
+    question.textContent = ans.question;
+    answer1.textContent = ans.answer1;
+    answer2.textContent = ans.answer2;
+    answer3.textContent = ans.answer3;
+    answer4.textContent = ans.answer4;
+}
+var before = questions.length - 1;
+var showing = 0;
 
-    // start timer upon clicking "Start Quiz"
+// click function
+startBtn.addEventListener("click", startGame)
 
-    getQuestion()
+function startGame() {
+    console.log('start')
+    startBtn.style.display = "none";
+    par.style.display = "none";
+    quest()
+    boxQuestions.style.display = "block"
+    counter()
 }
 
-function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex]
-    var currentChoices = questions[currentQuestionIndex].choices
+// scorebox holder function to display
+function scoreHolder() {
+    timeLeft.textContent = "You Scored..." + timeSec;
+    boxQuestions.style.display = "none"
+    nameholder.style.display = "block";
+}
 
-    var titleEl = document.getElementById("question-title")
-    titleEl.textContent = currentQuestion.question
-
-    var choicesEl = document.getElementById("choices")
-    console.log(currentChoices)
-
-   for(var i = 0; i < currentChoices.length; i++) {
-        var choiceSelection = document.createElement("button")
-        choiceSelection.setAttribute("class", "choice")
-        choiceSelection.setAttribute("value", currentChoices[i])
-
-        choiceSelection.textContent = i + 1 + ". " + currentChoices[i]
-
-        choicesEl.appendChild(choiceSelection)
+// function for right answers
+function correctAns(rightAns) {
+    if (rightAns === questions[showing].rightAns) {
+    } else {
+        timeSec -= 5;
     }
+    if (showing < before) {
+        showing++;
+        quest();
+    } else
+        scoreHolder();
 }
 
-// Here is where you will define an onclick to run the startQuiz function
+// function for the counter
+function counter() {
+    var count= setInterval(function () {
+     timeSec--;
+     var seconds = timeSec.toString();
+     timer.textContent = seconds;
+     if (timeSec <= 0) {
+         clearInterval(count);
+     } else if (before === showing) {
+         clearInterval(count)
+         
+     }
+ },1000)
+}
+
+// localStoage to add name to scoreholder
+
+// var highScores = Json.parse(localStorage.getItem(highScores)) || [];
+
+//     var score = {
+//         name
+
+//         score: recentScore
+
+
+
+
+
+
+
+
+
+
+
+
--- questions.js
+++ questions.js
@@ -5,17 +5,47 @@ var questions = [
         answer: "JavaScript"
     },
     {
-        question: "Is CSS fun",
+        question: "Is CSS fun?",
         choices: ["Yes", "No" ],
         answer: "No"
     },
     {
-        question: "What is the best programming language?",
-        choices: ["Java", "Go", "Ruby", "JavaScript"],
-        answer: "JavaScript"
+        question: "Where do we put the JavaScript link in HTML?",
+        choices: ["Head", "Body", "Div", "Header"],
+        answer: "Body"
     },{
-        question: "What is the best programming language?",
-        choices: ["Java", "Go", "Ruby", "JavaScript"],
-        answer: "JavaScript"
+        question: "What is a JavaScript keyword that refers to the object?",
+        choices: ["That", "Symbol", "String", "This"],
+        answer: "This"
     },
+    {
+        question: "This is the created when the page is loaded, and hold all the nodes and objects?",
+        choices: ["Head", "Body", "Dom", "Element"],
+        answer: "Dom"
+    },{
+        question: "Is JavaScript case-senitive?",
+        choices: ["Yes", "No"],
+        answer: "Yes"
+    },
+    {
+        question: "How do you start a FOR loop?",
+        choices: ["for i = 1 to 3", "for (i <= 3; i++)", "for (i = 0; i <= 3; i++)", "for (i = 0; i <= 3)"],
+        answer: "for (i = 0; i <= 3; i++)"
+    },{
+        question: "How would you create a function in JavaScript?",
+        choices: ["function = myFunction()", "function myFunction()", "function:myFunction()", "function myFunction{}"],
+        answer: "function myFunction()"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        choices: ["=", "+", "*", "%"],
+        answer: "="
+    },{
+        question: "The external JavaScript file must contain the <script> tag.",
+        choices: ["True", "False"],
+        answer: "True"
+    },
+
+
+
 ]
